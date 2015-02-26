2b88301535049253005e70b291262096c487e0e0
MalinKildal <malinkildal@gmail.com>
2015-02-26 13:48:16 +0100

    Update elevator.go

--- elevator.go
+++ elevator.go
@@ -14,44 +14,59 @@ var N_BUTTONS := 3;
 var N_FLOORS := 4;
 
 //Lager 4x3 lysmatrise:
-const lamp_channel_matrix := make ([][] int, N_FLOORS)
-for i := range lamp_channel_matrix {
-	a[i] = make([]int, N_BUTTONS)
+lamp_channel_matrix := make ([][] int, N_FLOORS)
+for i := 0; i<N_FLOORS; i++{
+	lamp_channel_matrix [i] = make([]int, N_BUTTONS)
 }
 
-//Implementerer lysmatrise for anntall etasjer:???? string med LIGHT_UP, LIGHT_DOWN og LIGHT_COMMAND med tall for antall etasjer?
-for i := range N_FLOORS {
-	for j := range N_BUTTONS {
-		if j=0 {
-			lamp_channel_matrix[i][j] = LIGHT_UP +j
-		}
-		else if j=1 {
-			lamp_channel_matrix[i][j] = LIGHT_DOWN +j
-		}
-		else if j=2 {
-			lamp_channel_matrix[i][j] = LIGHT_COMMAND +j
-		}
-	}
-}
+lamp_channel_matrix[0][0] = LIGHT_UP1
+lamp_channel_matrix[1][0] = LIGHT_UP2
+lamp_channel_matrix[2][0] = LIGHT_UP3
+lamp_channel_matrix[3][0] = LIGHT_UP4
+lamp_channel_matrix[0][1] = LIGHT_DOWN1
+lamp_channel_matrix[1][1] = LIGHT_DOWN2
+lamp_channel_matrix[2][1] = LIGHT_DOWN3
+lamp_channel_matrix[3][1] = LIGHT_DOWN4
+lamp_channel_matrix[0][2] = LIGHT_COMMAND1
+lamp_channel_matrix[1][2] = LIGHT_COMMAND2
+lamp_channel_matrix[2][2] = LIGHT_COMMAND3
+lamp_channel_matrix[3][2] = LIGHT_COMMAND4
 
-//Samme opplegg for button_channel_matrix
+
+//Lager 4x3 buttonmatrise:
 const button_channel_matrix := make ([][] int, N_FLOORS)
-for i := range button_channel_matrix {
-	a[i] = make([]int, N_BUTTONS)
+for i := 0; i<N_FLOORS; i++{
+	button_channel_matrix [i] = make([]int, N_BUTTONS)
 }
 
-//....implementer....
+button_channel_matrix[0][0] = BUTTON_UP1
+button_channel_matrix[1][0] = BUTTON_UP2
+button_channel_matrix[2][0] = BUTTON_UP3
+button_channel_matrix[3][0] = BUTTON_UP4
+button_channel_matrix[0][1] = BUTTON_DOWN1
+button_channel_matrix[1][1] = BUTTON_DOWN2
+button_channel_matrix[2][1] = BUTTON_DOWN3
+button_channel_matrix[3][1] = BUTTON_DOWN4
+button_channel_matrix[0][2] = BUTTON_COMMAND1
+button_channel_matrix[1][2] = BUTTON_COMMAND2
+button_channel_matrix[2][2] = BUTTON_COMMAND3
+button_channel_matrix[3][2] = BUTTON_COMMAND4
+
+/*
+button_channel_matrix = [[BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1],
+	[BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2],
+	[BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3],
+	[BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4]]
+*/
 
 
-
-
-func elev_init() int {			//bruker chan istedet for return
+func elev_init() int {
 
 	//Init hardware
 	if (!io_init())	{return 0}
 	
 	// Zero all floor button lamps
-	for i := range N_FLOORS {
+	for i := 0; i<N_FLOORS; i++ {
 		if i != 0 {
 			elev_set_button_lamp(BUTTON_CALL_DOWN, i, 0)
 		}
@@ -77,12 +92,10 @@ func elev_init() int {			//bruker chan istedet for return
 func elev_set_motor_direction(elev_motor_direvtion_t dirn) {
 	if dirn == 0 {
 		io_write_analog(MOTOR, 0)
-	}
-	else if dirn > 0 {
+	} else if dirn > 0 {
 		io_clear_bit(MOTORDIR)
 		io_write_analog(MOTOR, 2800)
-	}
-	else if dirn < 0 {
+	} else if dirn < 0 {
 		io_set_bit(MOTORDIR)
 		io_write_analog(MOTOR, 2800)
 	}
@@ -91,9 +104,10 @@ func elev_set_motor_direction(elev_motor_direvtion_t dirn) {
 
 func elev_set_door_open_lamp(value int) {
 	if value {
-		io_set_bit(LIGHT_DOOR_OPEN)}
-	else {
-		io_clear_bit(LIGHT_DOOR_OPEN)}
+		io_set_bit(LIGHT_DOOR_OPEN)
+	} else {
+		io_clear_bit(LIGHT_DOOR_OPEN)
+	}
 }
 
 
@@ -109,84 +123,104 @@ func elev_get_stop_signal() int {
 
 func elev_set_stop_lamp(value int) {
 	if value {
-		io_set_bit(LIGHT_STOP)}
-	else {
-		io_clear_bit(LIGHT_STOP)}
+		io_set_bit(LIGHT_STOP)
+	} else {
+		io_clear_bit(LIGHT_STOP)
+	}
 }
 
 
 func elev_get_floor_sensor_signal() int {
+
 	if (io_read_bit(SENSOR_FLOOR1)) {
-		return 0 }
-	else if (io_read_bit(SENSOR_FLOOR2)) {
-		return 1 }
-	else if (io_read_bit(SENSOR_FLOOR3)) {
-		return 2 }
-	else if (io_read_bit(SENSOR_FLOOR4)) {
-		return 3 }
-	else {
-		return -1 }
+		return 0 
+	} else if (io_read_bit(SENSOR_FLOOR2)) {
+		return 1 
+	} else if (io_read_bit(SENSOR_FLOOR3)) {
+		return 2 
+	} else if (io_read_bit(SENSOR_FLOOR4)) {
+		return 3 
+	} else {
+		return -1 
+	}
 }
 
 
 func elev_set_floor_indicator(int floor) {
-	assert(floor >= 0)
-	assert(floor < N_FLOORS)
-
+	
+	if floor < 0 {
+		Println ("Floor is lower than 0\n")
+		return 0 }
+	if floor > N_FLOORS {
+		Println ("Floor variable is too high\n")
+		return 0 }
+	
 	// Binary encoding. One light must always be on.
 	if (floor & 0x02){
-		io_set_bit(LIGHT_FLOOR_IND1)}
-	else {
-		io_clear_bit(LIGHT_FLOOR_IND1)}
+		io_set_bit(LIGHT_FLOOR_IND1)
+	} else {
+		io_clear_bit(LIGHT_FLOOR_IND1)
+	}
+		
 	if (floor & 0x01){
-		io_set_bit(LIGHT_FLOOR_IND2)}
-	else {
-		io_clear_bit(LIGHT_FLOOR_IND2)}
+		io_set_bit(LIGHT_FLOOR_IND2)
+	} else {
+		io_clear_bit(LIGHT_FLOOR_IND2)
+	}
 }
 
 
 func elev_get_button_signal(elev_button_type_t button, int floor, chan ch int) int {
 	
-	if floor >= 0 {
-		Println ("Floor is lower than 0\n")}
-	if floor < N_FLOORS {
-		Println ("Floor variable is too high\n")}
-	if !(button == BUTTON_CALL_UP && floor == N_FLOORS-1)) {
-		Println("Unvalid button call or floor\n")}
-	if !(button == BUTTON_CALL_DOWN && floor == 0)) {
-		Println("Unvalid button call or floor\n")}
-	if (button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND) {
-		Println("Unvalid button\n")}
-
+	if floor < 0 {
+		Println ("Floor is lower than 0\n")
+		return 0 }
+	if floor > N_FLOORS {
+		Println ("Floor variable is too high\n")
+		return 0 }
+	if (button == BUTTON_CALL_UP && floor == N_FLOORS-1)) {
+		Println("Unvalid button call or floor\n")
+		return 0 }
+	if (button == BUTTON_CALL_DOWN && floor == 0)) {
+		Println("Unvalid button call or floor\n")
+		return 0 }
+	if !(button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND) {
+		Println("Unvalid button\n")
+		return 0 }
+	
 
 	if (io_read_bit(button_channel_matrix[floor][button])){
-		return 1 }
-	else {
-		return 0 }
+		return 1
+	} else {
+		return 0 
+	}
 }
 
 
 
 func elev_set_button_lamp(elev_button_type_t button, int floor, int value) {
 
-	if floor >= 0 {
-		Println("Floor is negative\n")}
-	if floor < N_FLOORS {
-		Println("Floor variable is too high\n")}
-	if !(button == BUTTON_CALL_UP && floor == N_FLOORS-1)) {
-		Println("Unvalid button call or floor")}
-	if !(button == BUTTON_CALL_DOWN && floor == 0)) {
-		Println("Unvalid button call or floor\n")}
-	if (button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND) {
-		Println("Unvalid button\n")}
+	if floor < 0 {
+		Println("Floor is negative\n")
+		return }
+	if floor > N_FLOORS {
+		Println("Floor variable is too high\n")
+		return }
+	if (button == BUTTON_CALL_UP && floor == N_FLOORS-1)) {
+		Println("Unvalid button call or floor")
+		return }
+	if (button == BUTTON_CALL_DOWN && floor == 0)) {
+		Println("Unvalid button call or floor\n")
+		return }
+	if !(button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND) {
+		Println("Unvalid button\n")
+		return }
 
 	
 	if (value) {
-		io_set_bit(lamp_channel_matrix[floor][button])}
-	else {
-		io_clear_bit(lamp_channel_matrix[floor][button])}
+		io_set_bit(lamp_channel_matrix[floor][button])
+	} else {
+		io_clear_bit(lamp_channel_matrix[floor][button])
+	}
 }
-
-
-
 
